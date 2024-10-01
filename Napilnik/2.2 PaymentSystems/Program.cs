0f539e66893f8b02aff49d85858ddef1fe614805
Sterlios <kuzmin.s.anton@gmail.com>
2024-10-01 19:39:24 +0200

    2.2 PaymentSystem

--- Napilnik/2.2 PaymentSystems/Program.cs
+++ Napilnik/2.2 PaymentSystems/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System.Security.Cryptography;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Order order = new Order(268, 12000);
+        Order order = new Order(268, 12000, Currency.Rub);
+        Order order1 = new Order(268, 50, Currency.Eur);
+        Order order2 = new Order(268, 150, Currency.Usd);
 
-        //Выведите платёжные ссылки для трёх разных систем платежа: 
-        //pay.system1.ru/order?amount=12000RUB&hash={MD5 хеш ID заказа}
-        //order.system2.ru/pay?hash={MD5 хеш ID заказа + сумма заказа}
-        //system3.com/pay?amount=12000&curency=RUB&hash={SHA-1 хеш сумма заказа + ID заказа + секретный ключ от системы}
+        HashGenerator hashGenerator1 = new HashGenerator(MD5.Create());
+        HashGenerator hashGenerator2 = new HashGenerator(SHA1.Create());
+
+        PaymentSystem1 paymentSystem1 = new PaymentSystem1(hashGenerator1);
+        PaymentSystem2 paymentSystem2 = new PaymentSystem2(hashGenerator1);
+        PaymentSystem3 paymentSystem3 = new PaymentSystem3(hashGenerator2);
+
+        Console.WriteLine(paymentSystem1.GetPayingLink(order));
+        Console.WriteLine(paymentSystem2.GetPayingLink(order));
+        Console.WriteLine(paymentSystem3.GetPayingLink(order));
+        Console.WriteLine();
+        Console.WriteLine(paymentSystem1.GetPayingLink(order1));
+        Console.WriteLine(paymentSystem2.GetPayingLink(order1));
+        Console.WriteLine(paymentSystem3.GetPayingLink(order1));
+        Console.WriteLine();
+        Console.WriteLine(paymentSystem1.GetPayingLink(order2));
+        Console.WriteLine(paymentSystem2.GetPayingLink(order2));
+        Console.WriteLine(paymentSystem3.GetPayingLink(order2));
     }
 }
 
@@ -17,8 +34,40 @@ public class Order
 {
     public readonly int Id;
     public readonly int Amount;
+    public readonly Currency Currency;
 
-    public Order(int id, int amount) => (Id, Amount) = (id, amount);
+    public Order(int id, int amount, Currency currency)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(id);
+        ArgumentOutOfRangeException.ThrowIfNegative(amount);
+
+        if (Enum.IsDefined(currency) == false)
+            throw new ArgumentException(nameof(currency));
+
+        Id = id;
+        Amount = amount;
+        Currency = currency;
+    }
+}
+
+public class HashGenerator
+{
+    private readonly HashAlgorithm _algorithm;
+
+    public HashGenerator(HashAlgorithm algorithm) =>
+        _algorithm = algorithm ?? throw new ArgumentNullException(nameof(algorithm));
+
+    public string GetHash(params string[] data)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        foreach (string line in data)
+            stringBuilder.Append(line);
+
+        byte[] hash = _algorithm.ComputeHash(Encoding.ASCII.GetBytes(stringBuilder.ToString()));
+
+        return Encoding.ASCII.GetString(hash);
+    }
 }
 
 public interface IPaymentSystem
@@ -26,43 +75,60 @@ public interface IPaymentSystem
     public string GetPayingLink(Order order);
 }
 
-public abstract class PaymentSystem : IPaymentSystem
+public class PaymentSystem1 : IPaymentSystem
 {
-    private readonly HashAlgorithm _algorithm;
+    private readonly HashGenerator _hashGenerator;
 
-    public PaymentSystem(HashAlgorithm algorithm) =>
-        _algorithm = algorithm ?? throw new ArgumentNullException(nameof(algorithm));
+    public PaymentSystem1(HashGenerator hashGenerator) =>
+        _hashGenerator = hashGenerator ?? throw new ArgumentNullException(nameof(hashGenerator));
 
-    public abstract string GetPayingLink(Order order);
-}
-
-public class PaymentSystem1 : PaymentSystem
-{
-    public PaymentSystem1(HashAlgorithm algorithm) : base(algorithm)
+    public string GetPayingLink(Order order)
     {
-    }
+        ArgumentNullException.ThrowIfNull(order);
 
-    //pay.system1.ru/order?amount=12000RUB&hash={MD5 хеш ID заказа}
-    public override string GetPayingLink(Order order)
-    {
+        string hash = _hashGenerator.GetHash(order.Id.ToString());
 
+        return $"pay.system1.ru/order?amount={order.Amount}{order.Currency.ToString().ToUpper()}&hash={hash}";
     }
 }
 
-public class PaymentSystem2 : PaymentSystem
+public class PaymentSystem2 : IPaymentSystem
 {
-    public PaymentSystem2(HashAlgorithm algorithm) : base(algorithm)
-    {
-    }
+    private readonly HashGenerator _hashGenerator;
 
-    public override string GetPayingLink(Order order) => throw new NotImplementedException();
+    public PaymentSystem2(HashGenerator hashGenerator) =>
+        _hashGenerator = hashGenerator ?? throw new ArgumentNullException(nameof(hashGenerator));
+
+    public string GetPayingLink(Order order)
+    {
+        ArgumentNullException.ThrowIfNull(order);
+
+        string hash = _hashGenerator.GetHash(order.Id.ToString());
+
+        return $"order.system2.ru/pay?hash={hash}";
+    }
 }
 
-public class PaymentSystem3 : PaymentSystem
+public class PaymentSystem3 : IPaymentSystem
 {
-    public PaymentSystem3(HashAlgorithm algorithm) : base(algorithm)
-    {
-    }
+    private readonly HashGenerator _hashGenerator;
 
-    public override string GetPayingLink(Order order) => throw new NotImplementedException();
+    public PaymentSystem3(HashGenerator hashGenerator) =>
+        _hashGenerator = hashGenerator ?? throw new ArgumentNullException(nameof(hashGenerator));
+
+    public string GetPayingLink(Order order)
+    {
+        ArgumentNullException.ThrowIfNull(order);
+
+        string hash = _hashGenerator.GetHash(order.Id.ToString());
+
+        return $"system3.com/pay?amount={order.Amount}&curency={order.Currency.ToString().ToUpper()}&hash={hash}";
+    }
+}
+
+public enum Currency
+{
+    Rub,
+    Eur,
+    Usd
 }
